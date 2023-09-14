a840a6377ae9ee7dbb45958f6893028c4a1c96ab
jidesam <olajidesamson017@gmail.com>
2023-09-14 19:58:49 +0100

    'ui-update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamsComponent } from './pages/main/teams/teams.component';
+import { EmailComponent } from './email/email.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamsComponent
+    TeamsComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/main/landing/landing.component.ts
+++ src/app/pages/main/landing/landing.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+
+constructor(){
+
+}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  email(){
+    const emailAdress = 'info@en-turquoiseglobalservices.com'
+    window.location.href =`mailto: ${emailAdress}`
+  }
 
 }
