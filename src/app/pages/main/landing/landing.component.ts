f6b6b8f3589d317b33bedbd704dfa64d4c3c307f
BigAdroit <adroit819@gmail.com>
2023-09-25 13:38:35 +0100

    email update

--- src/app/pages/main/landing/landing.component.ts
+++ src/app/pages/main/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+declare let Email: any;
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
+  sendMessage() {
+      // const {email }= this.newsletterForm.value
+      console.log("test")
+        Email?.send({
+          Host: "smtp.elasticemail.com",
+          // port : "2525",
+          Username: "test@en-turquoiseglobalservices.com",
+          Password: "DAA91B70F71CC4CD612794C963DB1C23AFFD",
+          To: "adroit819@gmail.com",
+          From: 'test@en-turquoiseglobalservices.com',
+          Subject: "Add me to your newsletter",
+          Body: "Well that was easy!!"
+        })
+        .then( ()=> {
+            // this.notification.publishMessages("Thank you for subscribing to our newsletter!", "")
+            console.log("it works")
+            // setTimeout(() => {
+            //   // this.router.navigate(["/success"])
+            // }, 2500);
+          });  
+
+      // console.log(this.newsletterForm)
+  }
 }
