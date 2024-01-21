6b958851ca673874b3615a0da12bedb314b561f1
jidesam <olajidesamson017@gmail.com>
2024-01-21 16:41:36 +0100

    'update'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'en_turquoise-global';
+  title = 'en-turquoise-global';
 }
